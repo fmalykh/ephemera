64419ddc327e6cc909aa7c82268d42f2aa78b263
fmalykh <49435326+fmalykh@users.noreply.github.com>
2020-08-23 20:05:01 +0300

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -17,10 +17,10 @@ variable "app_name" {
 }
 
 variable "bucket_name" {
-  description = "Name of an S3 bucket in website mode.\nIf you plan to proxy it through Cloudflare, bucket name must match FQDN of the DNS record.\nhttps://support.cloudflare.com/hc/en-us/articles/360037983412-Configuring-an-Amazon-Web-Services-static-site-to-use-Cloudflare\nThis is also needed to confiure CORS."
+  description = "Name of an S3 bucket in website mode.\nIf you plan to proxy it through Cloudflare, bucket name must match FQDN of the DNS record.\nhttps://support.cloudflare.com/hc/en-us/articles/360037983412-Configuring-an-Amazon-Web-Services-static-site-to-use-Cloudflare\nThis is also needed to confiure CORS"
 }
 
 variable "scheme" {
-  description = "http|https\nIf you plan to use ephemera purely through S3 website, put http."
+  description = "http|https\nIf you plan to use ephemera purely through S3 website, put http"
 }
 
